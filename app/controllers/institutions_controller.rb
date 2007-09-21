4b7c8c7de079aca8309cb9c466c3000def1c29de
Joao Carlos Santi Junior <jr16rp@yahoo.com.br>
2007-09-21 20:09:31 +0000

    Criando o cadastro da instituição no controlle e na view
    git-svn-id: https://192.168.0.27:8443/svn/lamina/trunk@19 1fbeaf92-cb38-0410-906a-ebf9f3d3123e

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -4,4 +4,18 @@ class InstitutionsController < ApplicationController
     @institution = Institution.new
   end
 
+  def index
+    @institution_pages, @institutions = paginate :institutions, :per_page => 10
+  end
+
+  def create
+    @institution = Institution.new(params[:institution])
+    if @institution.save
+      flash[:notice] = 'Institicao criada com sucesso.'
+      redirect_to :action => 'index'
+    else
+      render :action => 'new'
+    end
+  end
+
 end

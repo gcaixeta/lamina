b8d8262a5c3716e06344af58d15eb74e1791da54
Gustavo Caixeta <gustavocaixeta@gmail.com>
2007-09-26 00:40:56 +0000

    Mudanca na Index do route, configuracao do home da aplicacao
    git-svn-id: https://192.168.0.27:8443/svn/lamina/trunk@30 1fbeaf92-cb38-0410-906a-ebf9f3d3123e

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
   # map.connect '', :controller => "welcome"
-  map.connect '', :controller => 'site', :action => 'index'
+  # map.connect '', :controller => 'site', :action => 'index'
+
+  map.home '', :controller => 'site', :action => 'index'
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
 

ee6f95de8b62625850103d46f7cef4ceef04ef92
Joao Carlos Santi Junior <jr16rp@yahoo.com.br>
2007-09-29 14:05:55 +0000

    Alteracao no test unitario do usuario alguns test estao repetidos
    git-svn-id: https://192.168.0.27:8443/svn/lamina/trunk@41 1fbeaf92-cb38-0410-906a-ebf9f3d3123e

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -7,73 +7,73 @@ class UserTest < Test::Unit::TestCase
   fixtures :users, :cities
 
   def test_should_be_invalid
-    user = create(:name => nil, :email => nil, :login => nil, :city_id => 1)
+    user = create_user(:name => nil, :email => nil, :login => nil, :city_id => 1)
     assert_invalid user, "User shouldn't be created"
   end
   
      # validar o name do user
  def test_should_require_name
-    user = create(:name => nil)
+    user = create_user(:name => nil)
     assert user.errors.invalid?(:name), ":name should be required"
     assert_invalid user, "User shouldn't be created"
   end
   
     # validar o city_id do user
   def test_should_require_city_id
-    user = create(:city_id => nil)
+    user = create_user(:city_id => nil)
     assert user.errors.invalid?(:city_id), ":city_id should be required"
     assert_invalid user, "User shouldn't be created"
   end
 
    # validar o email do user
   def test_should_require_email
-    user = create(:email => nil)
+    user = create_user(:email => nil)
     assert user.errors.invalid?(:email), ":email should be required"
     assert_invalid user, "User shouldn't be created"
   end
   
    # validar o formato do email do user
   def test_should_deny_bad_email
-    user = create(:email => 'bad@format')
+    user = create_user(:email => 'bad@format')
     assert user.errors.invalid?(:email), ":email should be in a valid format"
     assert_invalid user, "User shouldn't be created"
   end
 
    # nao permite a criacao duplicada de usuario
   def test_should_deny_duplicate_user
-    user = create
+    user = create_user
     assert_valid user
     
-    user = create
+    user = create_user
     assert_invalid user, "User shouldn't be created"
   end
   
 	# validar o int do codigo city_id da tabela user
   def test_should_deny_non_integer_city
-  user = create(:city_id => 'a')
+  user = create_user(:city_id => 'a')
   assert user.errors.invalid?(:city_id), ":city_id should have had an error"
   assert_invalid user, "user shouldn't be created"
   
-  user = create(:city_id => 1.397)
+  user = create_user(:city_id => 1.397)
   assert user.errors.invalid?(:city_id), ":city_id should have had an error"
   assert_invalid user, "user shouldn't be created"
   end
 
 def test_should_check_user_authorship
   # check all fixtures were loaded
-  assert_equal 2, cities(:rp).users.size, "cities should have had 2 users"
+  assert_equal 4, cities(:rp).users.size, "cities should have had 2 users"
  
   # assign a post without city_id
-  user = create(:city_id => nil)
+  user = create_user(:city_id => nil)
   
   # then, assign a user using the relationship method
   cities(:rp).users << user
   
   #now, check if city have one more user
-  assert_equal 3, cities(:rp).users.size, "city should have had 3 users"
+  assert_equal 5, cities(:rp).users.size, "city should have had 3 users"
   
   # assign a city to a user that doesn't exist
-  user = create(:city_id => 100)
+  user = create_user(:city_id => 100)
   assert user.errors.invalid?(:city), "city doesn't exist, so it should be required"
 end
 
@@ -171,19 +171,11 @@ end
     def create_user(options = {})
       User.create({ 
         :login => 'quire', 
+			 :name => "Joao",
         :email => 'quire@example.com', 
         :password => 'quire', 
-        :password_confirmation => 'quire' 
-        }.merge(options))
-    end
-
-
-    def create(options={})
-      User.create({
-        :name => "Joao",
-        :email => "joao@terra.com.br",
-        :login => "jaum",
-	:city_id => 1
+        :password_confirmation => 'quire', 
+      	:city_id => 1
         }.merge(options))
     end
 

c19d281008044f75590fdcfc153bf07f0d7f47b7
assvn <assvn@1fbeaf92-cb38-0410-906a-ebf9f3d3123e>
2007-10-12 23:10:53 +0000

    Criacao do Themes com path_
    git-svn-id: https://192.168.0.27:8443/svn/lamina/trunk@95 1fbeaf92-cb38-0410-906a-ebf9f3d3123e

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -2,9 +2,19 @@ class ThemesController < ApplicationController
 before_filter :login_required,  :only => [ :new, :create ] 
 
 def new
+  #@theme = Theme.new
 end
 
 def create
-end
+    @theme = Theme.new(params[:theme])
+    configuration = Configuration.create(params[:configuration])
+    @theme.configuration_id = configuration.id
+    if @theme.save
+      flash[:notice] = 'Theme criada com sucesso.'
+      redirect_to :controller => 'site' , :action => 'index'
+    else
+      render :action => 'new'
+    end
+  end
 
 end
--- test/functional/themes_controller_test.rb
+++ test/functional/themes_controller_test.rb
@@ -5,7 +5,7 @@ require 'themes_controller'
 class ThemesController; def rescue_action(e) raise e end; end
 
 class ThemesControllerTest < Test::Unit::TestCase
-fixtures :users, :registrations, :institutions, :cities, :states, :profiles
+fixtures :users, :registrations, :institutions, :cities, :states, :profiles, :themes, :configurations, :proposals
   def setup
     @controller = ThemesController.new
     @request    = ActionController::TestRequest.new
@@ -37,6 +37,13 @@ fixtures :users, :registrations, :institutions, :cities, :states, :profiles
     assert_response :success           
     assert_equal 9,@request.session[:user]    
     assert_equal 2, Registration.find_by_user_id_and_institution_id(session[:user], 2).profile_id      
+  end
+
+  def test_should_login_and_redirect
+        login_as :usp1
+        old_count = Theme.count
+        post :create, :theme => {:name => 'Tecnologia',  :summary => 'Mundo Atual', :active => '1', :password => '1234'}, :configuration => {:questions_max => '10', :number_participants => 5}
+      assert_equal old_count +1 , Theme.count
   end
 
 end

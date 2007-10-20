792db58cb9cb6485f11844af856db6aecfb8e2b9
assvn <assvn@1fbeaf92-cb38-0410-906a-ebf9f3d3123e>
2007-10-20 02:43:33 +0000

    Crud do Link ainda precisa de arrumar algumas coisas na view
    git-svn-id: https://192.168.0.27:8443/svn/lamina/trunk@121 1fbeaf92-cb38-0410-906a-ebf9f3d3123e

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,16 +3,89 @@ class LinksController < ApplicationController
 
   before_filter :find_theme
 
+  # GET /link
+  # GET /links.xml
   def index
-    @links = @theme.links
+    @links = @theme.links.find(:all)
 
     respond_to do |format|
       format.html # index.rhtml
       format.xml  { render :xml => @links.to_xml }
-      format.rss  { render :xml => @theme.to_rss }
+    end
+  end
+  
+  # GET /links/1
+  # GET /links/1.xml
+  def show
+    @link = Link.find(params[:id])
+    
+    respond_to do |format|
+      format.html # show.rhtml
+      format.xml  { render :xml => @link.to_xml }
+    end
+  end
+  
+
+# GET /links/new
+  def new
+    @link = Link.new
+  end
+  
+
+  # GET /episodes/1;edit
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  # POST /links
+  # POST /links.xml
+  def create
+    @link = Link.new(params[:link])
+    
+    respond_to do |format|
+      if @link.save
+        flash[:notice] = 'Episode was successfully created.'
+        
+        format.html { redirect_to link_url(@link) }
+        format.xml do
+          headers["Location"] = link_url(@link)
+          render :nothing => true, :status => "201 Created"
+        end
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @link.errors.to_xml }
+      end
     end
   end
 
+  # PUT /links/1
+  # PUT /links/1.xml
+  def update
+    @link = Link.find(params[:id])
+    
+    respond_to do |format|
+      if @link.update_attributes(params[:link])
+        format.html { redirect_to link_url(@link) }
+        format.xml  { render :nothing => true }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @link.errors.to_xml }        
+      end
+    end
+  end
+  
+  # DELETE /links/1
+  # DELETE /links/1.xml
+  def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to links_url   }
+      format.xml  { render :nothing => true }
+    end
+  end
+  
 
 
 private
--- test/functional/links_controller_test.rb
+++ test/functional/links_controller_test.rb
@@ -13,15 +13,31 @@ fixtures :users, :links, :registrations, :institutions, :cities, :states, :profi
     @response   = ActionController::TestResponse.new
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
 
   def test_should_get_index
     get :index, :theme_id => 1
     assert_response :success
     assert_not_nil assigns(:links)
   end
+
+
+  def test_should_get_new
+    login_as 'usp1'
+    get :new, :theme_id => 1
+    assert_response :success
+  end
+
+
+  def test_should_create_episode
+    login_as 'usp1'
+    old_count = Link.count
+    post :create, :theme_id => 1, :link => { :address => "www.uol.com.br/tecnologia", :description => "tecno" }
+    assert_equal old_count+1, Link.count
+    
+    assert_redirected_to theme_url(assigns(:link).theme_id)
+  end
+
+
+
 
 end

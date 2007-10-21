5afe8beb61970eafd08e6a347c5d12ad334d30e9
Gustavo Caixeta <gustavocaixeta@gmail.com>
2007-10-21 19:50:58 +0000

    colocando filtro de permissao em grupos
    git-svn-id: https://192.168.0.27:8443/svn/lamina/trunk@141 1fbeaf92-cb38-0410-906a-ebf9f3d3123e

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,15 @@
 class GroupsController < ApplicationController
-
+  before_filter :login_required, :only => [ :index, :show ]
   before_filter :find_theme, :except => [:index, :show]
+  before_filter :have_permission_to_view, :only => [:show]
+  
 
 # p = Participation.find_all_by_group_id(1, :include=>[:interactions])
 # p = Participation.find_all_by_group_id(5, :include=>[:messages])
 
 
   def index
-    @groups = Group.find :all
+    @groups = Group.find(:all)
     respond_to do |format|
       format.html # index.rhtml
       format.xml  { render :xml => @groups.to_xml }
@@ -31,5 +33,13 @@ private
 
   def find_theme
     @theme = Theme.find params[:theme_id]
+  end
+  
+  def have_permission_to_view
+    #pega o id do grupo
+    group = Group.find(params[:id])
+    #verifica qual instituicao? e ve se o usuario tem registration nela?
+    #verifica se ele tem participation, ou proposal no theme do grupo
+    
   end
 end
--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -6,7 +6,7 @@ class GroupsController; def rescue_action(e) raise e end; end
 
 class GroupsControllerTest < Test::Unit::TestCase
 
-fixtures :registrations, :institutions, :profiles, :groups, :participations, :interactions
+fixtures :users, :registrations, :institutions, :profiles, :groups, :participations, :interactions
 
   def setup
     @controller = GroupsController.new
@@ -16,6 +16,7 @@ fixtures :registrations, :institutions, :profiles, :groups, :participations, :in
 
 # Dados para Testes
 # use 7 8
+# :usa3 :usa4
 # kiko nanda
 # reg 5 6
 # par 3 4
@@ -28,16 +29,33 @@ fixtures :registrations, :institutions, :profiles, :groups, :participations, :in
     assert true
   end
   
+  def test_get_index_and_require_login
+    get :index
+    assert_response :redirect
+  end
+  
+  def test_get_show_and_require_login
+    get :show, :id => 5
+    assert_response :redirect
+  end
+
   def test_get_index
+    login_as :usa3
     get :index
     assert_response :success
   end
   
   def test_get_show
-    get :show, :id => 1
+    login_as :usa3
+    get :show, :id => 5
     assert_response :success
   end
   
+    def test_get_show_without_permission
+    login_as :quentin
+    get :show, :id => 5
+    assert_response :success
+  end
   
   
   

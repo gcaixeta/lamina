4cefca1171b4b9be37c91cdd0cf573d284f678a9
Gustavo Caixeta <gustavocaixeta@gmail.com>
2007-10-25 22:35:36 +0000

    identando arquivo controlador de grupos, removendo partial bugada
    git-svn-id: https://192.168.0.27:8443/svn/lamina/trunk@175 1fbeaf92-cb38-0410-906a-ebf9f3d3123e

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,56 +4,52 @@ class GroupsController < ApplicationController
   before_filter :have_permission_to_view, :only => [:show]
   before_filter :have_proposal_in_theme, :only => [:new, :create, :update, :edit, :destroy]      
 
-# p = Participation.find_all_by_group_id(1, :include=>[:interactions])
-# p = Participation.find_all_by_group_id(5, :include=>[:messages])
+  # p = Participation.find_all_by_group_id(1, :include=>[:interactions])
+  # p = Participation.find_all_by_group_id(5, :include=>[:messages])
 
 
 
 
-#Planejamento de URL do Grupos
-#URLs de acesso do Professor
-#/themes/:theme_id/groups/new -get
-#/themes/:theme_id/groups/create - post
-#/themes/:theme_id/groups/edit - get
-#/themes/:theme_id/groups/update - put
-#/themes/:theme_id/groups/destroy - delete
-#/themes/:theme_id/groups/list - Listar por ultima interacao
+  #Planejamento de URL do Grupos
+  #URLs de acesso do Professor
+  #/themes/:theme_id/groups/new -get
+  #/themes/:theme_id/groups/create - post
+  #/themes/:theme_id/groups/edit - get
+  #/themes/:theme_id/groups/update - put
+  #/themes/:theme_id/groups/destroy - delete
+  #/themes/:theme_id/groups/list - Listar por ultima interacao
 
   def new
         
   end
 
   def create
-          
-          
-          group = Group.new(params[:group])
-          
-          if @theme.groups << group
-              @theme.save
-              participations = params[:participations]
+    group = Group.new(params[:group])
 
-if participations != nil     
+    if @theme.groups << group
+      @theme.save
+      participations = params[:participations]
 
-             logins = participations.split(', ')
-                            
-              institution_id = Proposal.find_by_theme_id(params[:theme_id]).registration.institution_id
-              for login in logins
-                  registrations = User.find_by_login(login).registrations.find_by_institution_id_and_profile_id(institution_id,1) #TODO verificar find, se nao deve colocar all
-                  if registrations != nil #colocar [] se for vetor
-                    group.registrations << registrations
-                  end
-              end
-end
-              # 1 - Localizar user_id
-              # 2 - Verificar se ele tem registration, na instituição do grupo (pegar por themes > proposal >registration_institution_id ?
-              # 3 - tem registration?
-              # 4 - cria as participations com o id da registration
-              # 5 fazer loop em todos os logins?
-              flash[:notice] = 'Grupo foi criado com sucesso.'
-              redirect_to list_theme_groups_path(params[:theme_id])          
-          else  
-              render :action => 'new'
+      if participations != nil     
+        logins = participations.split(', ')
+        institution_id = Proposal.find_by_theme_id(params[:theme_id]).registration.institution_id
+        for login in logins
+          registrations = User.find_by_login(login).registrations.find_by_institution_id_and_profile_id(institution_id,1) #TODO verificar find, se nao deve colocar all
+          if registrations != nil #colocar [] se for vetor
+            group.registrations << registrations
           end
+        end
+      end
+      # 1 - Localizar user_id
+      # 2 - Verificar se ele tem registration, na instituição do grupo (pegar por themes > proposal >registration_institution_id ?
+      # 3 - tem registration?
+      # 4 - cria as participations com o id da registration
+      # 5 fazer loop em todos os logins?
+      flash[:notice] = 'Grupo foi criado com sucesso.'
+      redirect_to list_theme_groups_path(params[:theme_id])          
+    else  
+      render :action => 'new'
+    end
   end
 
   def edit
@@ -86,7 +82,7 @@ end
   end
 
   def list
-  @groups = Group.find_all_by_theme_id(@theme)
+    @groups = Group.find_all_by_theme_id(@theme)
   end
 
 
@@ -114,12 +110,12 @@ end
   end
   
   def last_msgs
-  p = Participation.find_all_by_group_id(5, :include=>[:messages])
-  @messages = p.first.messages
-  page.insert_html :bottom, 'messages', :partial => 'message', :collection => @messages
+    p = Participation.find_all_by_group_id(5, :include=>[:messages])
+    @messages = p.first.messages
+    page.insert_html :bottom, 'messages', :partial => 'message', :collection => @messages
   end
 
-private
+  private
 
   def find_theme
     @theme = Theme.find params[:theme_id]
@@ -127,28 +123,28 @@ private
   
   def have_permission_to_view
     #pega o id do grupo
-      @group = Group.find(params[:id])
-      participations = Participation.find_all_by_group_id(@group, :include=>[:registration])
+    @group = Group.find(params[:id])
+    participations = Participation.find_all_by_group_id(@group, :include=>[:registration])
       
       
       
-      #loop do verification of participations of students
-      for participation in participations do
+    #loop do verification of participations of students
+    for participation in participations do
 
-       if participation.registration.user_id != session[:user]
-          autorized = false
-        else
-          autorized = true
-          break #stop the verification of participation / registration
-        end
+      if participation.registration.user_id != session[:user]
+        autorized = false
+      else
+        autorized = true
+        break #stop the verification of participation / registration
       end
+    end
 
 
     #TODO Otimizar isso...
     #se nao tiver autorizacao, verifica se é professor da thematica do grupo
     if autorized == false
       for proposal in @group.theme.proposals do
-       if proposal.registration.user_id != session[:user]
+        if proposal.registration.user_id != session[:user]
           autorized = false
         else
           autorized = true
@@ -158,23 +154,23 @@ private
     end
 
 
-      if autorized == false
-        flash[:error] = "Seu usuario não tem autorizacao entrar neste ambiente do grupo '#{@group.name}'"
-        redirect_to :controller => '/site', :action => 'index'
-      end
+    if autorized == false
+      flash[:error] = "Seu usuario não tem autorizacao entrar neste ambiente do grupo '#{@group.name}'"
+      redirect_to :controller => '/site', :action => 'index'
+    end
 
     
   end
 
 
-       def have_proposal_in_theme
-         #filtro para identificar se o professor tem um proposals senao ele nao podera edit e update
+  def have_proposal_in_theme
+    #filtro para identificar se o professor tem um proposals senao ele nao podera edit e update
 
-     registrations = Theme.find(@theme).registrations.find_all_by_user_id_and_profile_id(session[:user], 2)   
-     if registrations == []
-          flash[:error] = "Não tem permissao para criar um grupo'"
-          redirect_to themes_path
-        end 
+    registrations = Theme.find(@theme).registrations.find_all_by_user_id_and_profile_id(session[:user], 2)   
+    if registrations == []
+      flash[:error] = "Não tem permissao para criar um grupo'"
+      redirect_to themes_path
+    end 
 
-       end
+  end
 end

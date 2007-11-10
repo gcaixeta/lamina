f2389f8e608e4729ceeb7819b4e7c52d9546f5b8
Joao Carlos Santi Junior <jr16rp@yahoo.com.br>
2007-11-10 12:18:48 +0000

    parte quase feita
    git-svn-id: https://192.168.0.27:8443/svn/lamina/trunk@286 1fbeaf92-cb38-0410-906a-ebf9f3d3123e

--- config/routes.rb
+++ config/routes.rb
@@ -47,13 +47,16 @@ ActionController::Routing::Routes.draw do |map|
     groups.resources :messages, :collection => {:list =>:post}
     groups.resources :interactions, :collection => {:list =>:post}
     groups.resources :activities, :member => {:approved => :put}
-    groups.resources :activities, :name_prefix => "group_"
+    groups.resources :activities, :name_prefix => "observation_"
     groups.resources :answers
     groups.resources :questions
     groups.resources :plays, :collection => {:list =>:post}
     groups.resources :games, :member => {:play => :get}
   end
 
+map.resources :activities do |activities|
+activities.resources :observations
+end
 
 
 
